e90be4eb4d35b0426ab729dd0c769988e95d72c6
Ryan Zemlin <rzemlin@gmail.com>
2022-06-20 21:22:29 -0700

    npm fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './components/Home.js';
+import NavBar from './components/NavBar.js';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+        <Router>
+          <div className="App">
+            <NavBar />
+            <div className='center-card'>
+            <Routes>
+              <Route path="/" element={Home} />
+            </Routes>
+            </div>
+          </div>
+        </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+export default function NavBar() {
+    return 
+    <NavLink to="/">Home</NavLink>
+  }
