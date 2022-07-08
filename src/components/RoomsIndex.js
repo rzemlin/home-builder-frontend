b3d4892f99caf4ace483b51482d410534e40e9f8
Ryan Zemlin <rzemlin@gmail.com>
2022-07-08 09:08:52 -0700

    fix null value for map function

--- src/components/RoomsIndex.js
+++ src/components/RoomsIndex.js
@@ -1,13 +1,12 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
 
-const RoomsIndex = (props) => {
-    console.log(props)
+const RoomIndex = (props) => {
     return <div>
-        <h3>Current Renovations:</h3>
+        <h2>Current Rooms:</h2>
         {props.rooms.map(room => (
             <div key={room.id}>
                 <Link to={`/rooms/${room.id}`} >                   
+                    {/* <h3 onClick={(() => {props.getProject(project.id)})}>{project.title}</h3> */}
                     <h3>{room.room_name}</h3>
                 </Link>
                 <button onClick={(() => {props.deleteRoom(room.id)})}>Delete Room</button>
@@ -17,4 +16,4 @@ const RoomsIndex = (props) => {
     </div>
 }
 
-export default RoomsIndex;
+export default RoomIndex;
